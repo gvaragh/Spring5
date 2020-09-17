8e329123ec5b4e10692b22f4435c04a162c2d1ed
Vasudevan G <gvaragh@gmail.com>
2020-09-17 17:07:27 +0800

    Spring5 Initializr

---
+++ src/main/java/guru/springframework/spring5webapplication/Spring5webapplicationApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5webapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5webapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/spring5webapplication/Spring5webapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5webapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
